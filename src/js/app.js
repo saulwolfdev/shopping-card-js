8da9669316fe15fd7d7f7e2fe0fb469883bca4ab
saul garay <estrellasoscuras1984@gmail.com>
2021-02-21 18:32:55 -0300

    Carrito de compra completo, sin estilos

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,107 @@
-
-
 const card = document.querySelector(".Shopping_Card")
 const containerShopping = document.querySelector(".Shopping_Card_List")
 const clearShopping = document.querySelector(".Shopping_Button")
+const listCards = document.querySelector("#Contanier_Cards")
 
-const listCards = document.querySelector(".Card_Button")
+let articlesCards = [];
 
 chargeEventListener()
+
 function chargeEventListener() {
-  listCards.addEventListener("click",addCourses)
+  listCards.addEventListener("click", addCards);
+  card.addEventListener("click", deleteCards)
+  
+  clearShopping.addEventListener("click",clearShoppingCard)
+
 }
 
-function addCourses(e) {
+function addCards(e) {
   e.preventDefault()
   if (e.target.classList.contains("Card_Button")) {
     const cardSelected = e.target.parentElement;
-    console.log("se agrego al carrito")
-    console.log(cardSelected)
-     
     readCardElementHtml(cardSelected)
   }
 }
 
-function readCardElementHtml(card) {
-  console.log(card)
+function readCardElementHtml(cardSelectRead) {
+
+  const infoSelectCard = {
+    img: cardSelectRead.querySelector(".Card_Image").src,
+    title: cardSelectRead.querySelector(".Card_Title").textContent,
+    price: cardSelectRead.querySelector(".Card_Price").textContent,
+    id: cardSelectRead.querySelector("a").getAttribute("data-id"),
+    cant:1
+  }
+  //verification duplicates element cards//
+  const checkCardsDuplicateSelected = articlesCards.some(cardSelectRead => cardSelectRead.id === infoSelectCard.id)
+
+  // const checkCardsDuplicateSelected = articlesCards.some((cardSelectRead) => {
+  //     return cardSelectRead.id === infoSelectCard.id
+  // })
+
+  if (checkCardsDuplicateSelected) {
+    const verificationDuplicatesId = articlesCards.map((cardSelectReadId) => {
+      if (cardSelectReadId.id === infoSelectCard.id) {
+        cardSelectReadId.cant++;
+        return cardSelectReadId
+      }
+      else {
+        return cardSelectReadId
+      }
+    })
+    articlesCards=[...verificationDuplicatesId]
+  } else {
+    articlesCards = [
+      ...articlesCards, infoSelectCard
+    ]
+  }
+
+  console.log('articlesCards :>> ', articlesCards);
+
+  generateShoppingCardHtml();
+}
+
+function generateShoppingCardHtml() {
+  
+  clearShoppingCardHtml()
+
+  articlesCards.forEach((articleCard) => {
+    const divContainer = document.createElement("div")
+
+    const { img, title, price, id, cant } = articleCard;
+    divContainer.innerHTML = `
+    <div class="Shopping_Card_List_Items">
+      <img src="${img}" alt="${title}"/>
+     <h3>${title}</h3>
+      ${price}
+      ${cant}
+      <a  class="Shopping_Delete" href="#" data-id="${id}">X</a>
+    </div>
+    `;
+    containerShopping.appendChild(divContainer);
+  
+  })
+}
+
+function clearShoppingCardHtml() {
+  // containerShopping.innerHTML = "";
+  while (containerShopping.firstChild) {
+    containerShopping.removeChild(containerShopping.firstChild)
+  }
+}
+
+function deleteCards(e) {
+  // console.log(e.target.classList.contains("Shopping_Delete"))
+  if (e.target.classList.contains("Shopping_Delete")) {
+    const deleteCardId = e.target.getAttribute("data-id")
+    //delete por ID
+    articlesCards = articlesCards.filter(articleCard => articleCard.id !== deleteCardId)
+    console.log("DELETE", articlesCards);
+    generateShoppingCardHtml();
+  }
+}
+
+function clearShoppingCard() {
+  articlesCards = [];
+  clearShoppingCardHtml()
 }
